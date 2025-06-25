a157924214019b78935293e9691783712bd2d2ad
NallamoluNagaDivya <nallamolusrisanthi@gmail.com>
2025-06-25 15:49:16 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import AdminDashboard from './components/AdminDashboard';
-import UserBookings from './components/UserBookings';
+import FlightSearch from './FlightSearch';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/admin" element={<AdminDashboard />} />
-        <Route path="/my-bookings" element={<UserBookings />} />
-      </Routes>
-    </Router>
+    <div>
+      <FlightSearch />
+    </div>
   );
 }
 
